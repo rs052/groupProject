b262f0cdd5bd90b7dfbdb050f1fb58ae8e89c77f
rs052 <riya.shah081@gmail.com>
2025-07-08 11:44:28 -0400

    Update Caterpillar.kt

--- Caterpillar.kt
+++ Caterpillar.kt
@@ -7,9 +7,15 @@ class Caterpillar {
     private lateinit var headRect : Rect
     private var speed : Int = 10
     private var direction : String = "up"
+    private var gameOver : Boolean = false
+    private var screenWidth : Float = 0f
+    private var screenHeight : Float = 0f
 
-    constructor(headRect : Rect) {
+
+    constructor(headRect : Rect, width: Int, height: Int) {
         this.headRect = headRect
+        this.screenWidth = width.toFloat()
+        this.screenHeight = height.toFloat()
     }
 
     fun setDirection(s : String) {
@@ -19,7 +25,15 @@ class Caterpillar {
     fun getDirection() : String {
         return direction
     }
+
+    fun isGameOver () : Boolean {
+        return gameOver
+    }
+
     fun moveCaterpillar() {
+        if (gameOver) {
+            return
+        }
         if (direction == "up") {
             headRect.offset(0, -speed)
         }
@@ -31,6 +45,11 @@ class Caterpillar {
         }
         if (direction == "right") {
             headRect.offset(speed,0)
+        }
+
+        if (headRect.left < 0 || headRect.right > screenWidth || headRect.bottom > screenHeight || headRect.top < 0) {
+            headRect.offset(0,0)
+            gameOver = true
         }
     }
 }
